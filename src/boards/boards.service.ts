c723439b4037019292e4a1468637e73a97cc636c
Maksym Diachok <maksdyachok2005@gmail.com>
2025-07-26 15:23:50 +0300

    Create admin role with board

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -3,16 +3,29 @@ import { CreateBoardDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { plainToInstance } from 'class-transformer';
 import { BoardDto } from './dto/board-dto';
+import { Role } from 'generated/prisma';
 
 @Injectable()
 export class BoardsService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createBoard(createBoardDto: CreateBoardDto) {
-    console.log(createBoardDto);
+  async createBoard(createBoardDto: CreateBoardDto, userId: string) {
     const board = await this.prismaService.board.create({
-      data: { ...createBoardDto },
-    });
+      data: {
+        ...createBoardDto
+      }
+    })
+    this.prismaService.membership.create({
+      data: {
+        user: { connect: { id: userId }},
+        board: {
+          connect : {
+            id: board.id,
+          }
+        },
+        role: Role.ADMIN,
+      }
+    })
 
     return plainToInstance(BoardDto, board, { excludeExtraneousValues: true });
   }
