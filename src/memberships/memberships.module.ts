b602cedf62931eee98de4e9d55e1a0c238978e23
Maksym Diachok <maksdyachok2005@gmail.com>
2025-08-08 19:28:20 +0300

    Fix for guard usage in task module

--- src/memberships/memberships.module.ts
+++ src/memberships/memberships.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MembershipsService } from './memberships.service';
 import { MembershipsController } from './memberships.controller';
+import { BoardAccessGuard } from './guards';
 
 @Module({
-  providers: [MembershipsService],
-  exports: [MembershipsService],
+  providers: [MembershipsService, BoardAccessGuard],
+  exports: [MembershipsService, BoardAccessGuard],
   controllers: [MembershipsController],
 })
 export class MembershipsModule {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { MembershipsModule } from 'src/memberships/memberships.module';
 
 @Module({
   controllers: [TasksController],
   providers: [TasksService],
+  imports: [MembershipsModule],
 })
 export class TasksModule {}
